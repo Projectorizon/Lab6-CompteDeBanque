6cefb21df6a92c3585f9662bf3ca3bf347334bb9
Remy Giard <rgiard@gmail.com>
2023-02-06 08:50:35 -0500

    Ajout des operateurs << et >>

--- main.cpp
+++ main.cpp
@@ -19,10 +19,12 @@ int main()
 
 	compteDeAlain += chequeSalaire;  // le solde du compte de banque doit être augmenter du montant du chèque 
 
+	cout << "Paiement de " << paiementHydro << endl;
 	if (compteDeAlain >= paiementHydro) { // le solde du compte de banque est comparé à celui du chèque
 		compteDeAlain -= paiementHydro; // le solde du compte de banque doit être réduit du montant du chèque
 	}
 
+	cout << "Paiement de " << carteDeCredit << endl;
 	if (compteDeAlain > carteDeCredit) { // le solde du compte de banque est comparé à celui de la carte de crédit
 		compteDeAlain -= carteDeCredit; // le compte de banque doit être déduit du solde de la carte de crédit 
 		carteDeCredit = 0; // le solde de la carte de crédit est mis à zéro
@@ -30,6 +32,9 @@ int main()
 
 	compteDeAlain += 100; // le compte de banque doit être augmenter du montant recu 
 
+	compteDeAlain++;
+
+	cout << transfert << endl;
 	CompteBanque compteDeJosee(6500);
 	compteDeJosee += transfert; // ajout le montant du transfert dans du solde du compte de Josée
 	compteDeAlain -= transfert;  // enleve le montant du transfert du solde du compte de Alain
@@ -49,7 +54,13 @@ int main()
 	compteDeAlain = 0; // met le solde du compte de Alain à 0
 	compteDeJosee = 0;  // met le solde du compte de Josee à 0
 
-	cout << "Le compte conjoint a un solde de " << compteConjoint.getSolde() << "$" << endl;
+	cout << "Le compte conjoint a un solde de " << compteConjoint << "$" << endl;
+
+	cout << "Entrer le solde du nouveau compte: " << endl;
+
+	CompteBanque nouveauCompte;
+	cin >> nouveauCompte;
+	cout << "Solde du nouveau compte: " << nouveauCompte;
 
 	return 0;
 }
